add5d8c1e1eb4719b821bb599032f902877a5855
“manan-bansal” <mananbansal2808@gmail.com>
2022-04-16 19:02:18 +0530

    Updated files

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:8
 MAINTAINER Manan Bansal mananbansal2808@gmail.com
-COPY ./target/calculator-devops-1.0-SNAPSHOT-jar-with-dependencies.jar ./
+COPY ./target/ScientificCalculator-1.0-SNAPSHOT-jar-with-dependencies.jar ./
 WORKDIR ./
-CMD ["java", "-jar", "calculator-devops-1.0-SNAPSHOT-jar-with-dependencies.jar"]
+CMD ["java", "-jar", "ScientificCalculator-1.0-SNAPSHOT-jar-with-dependencies.jar"]
--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -21,9 +21,9 @@ public class Calculator {
         Scanner scanner = new Scanner(System.in);
         double number1, number2 = 0;
         do {
-            System.out.println("Calculator-DevOps, Choose to perform operation");
-            System.out.print("Press 1 to Add\nPress 2 to Subtract\nPress 3 to Multiply\nPress 4 to Divide\nPress 5 to Inverse\nPress 6 for Factorial\nPress 7 to Power\nPress 8 to find Square Root\nPress 9 to find Log\n" +
-                    "Press any other key to exit\nEnter your choice: ");
+            System.out.println("Scientific Calculator : Press the number corresponding to the operation that you need to perform");
+            System.out.print("1) Add\n2) Subtract\n3) Multiply\n4) Divide\n5) Inverse\n6) Factorial\n7) Power\n8) Square Root\n9) Log\n" +
+                    "Press any other key to exit");
             int choice;
             try {
                 choice = scanner.nextInt();
@@ -31,7 +31,7 @@ public class Calculator {
                 return;
             }
             if((choice==5) || (choice==6) || (choice==8) || (choice==9)) {
-                try{
+                try{// do subtraction
                     System.out.print("Enter the number : ");
                     number1 = scanner.nextDouble();
                 } catch (InputMismatchException error) {
@@ -45,49 +45,40 @@ public class Calculator {
                     System.out.print("Enter the second number : ");
                     number2 = scanner.nextDouble();
                 } catch (InputMismatchException error) {
-                    logger.error("Invalid input, Entered input is not a number");
+                    logger.error("Invalid input");
                     return;
                 }
             }
             switch (choice) {
                 case 1:
-                    // do addition
-                    System.out.println("Addition result is : " + calculator.add(number1, number2));
+                    System.out.println("Addition : " + calculator.add(number1, number2));
                     break;
                 case 2:
-                    // do subtraction
-                    System.out.println("Subtraction result is : " + calculator.subtract(number1, number2));
+                    System.out.println("Subtraction : " + calculator.subtract(number1, number2));
                     break;
                 case 3:
-                    // do multiplication
-                    System.out.println("Multiplication result is : " + calculator.multiply(number1, number2));
+                    System.out.println("Multiplication : " + calculator.multiply(number1, number2));
                     break;
                 case 4:
-                    // do division
-                    System.out.println("Division result is : " + calculator.divide(number1, number2));
+                    System.out.println("Division result : " + calculator.divide(number1, number2));
                     break;
                 case 5:
-                    // do inverse
-                    System.out.println("Inverse result is : " + calculator.inverse(number1));
+                    System.out.println("Inverse result : " + calculator.inverse(number1));
                     break;
                 case 6:
-                    // do factorial
-                    System.out.println("Factorial result is : " + calculator.factorial(number1));
+                    System.out.println("Factorial result : " + calculator.factorial(number1));
                     break;
                 case 7:
-                    // do x power y
-                    System.out.println("X power Y is : " + calculator.powered(number1, number2));
+                    System.out.println("X power Y : " + calculator.powered(number1, number2));
                     break;
                 case 8:
-                    // do square root
-                    System.out.println("Square root is : " + calculator.root(number1));
+                    System.out.println("Square root : " + calculator.root(number1));
                     break;
                 case 9:
-                    // do log
-                    System.out.println("Log is : " + calculator.log(number1));
+                    System.out.println("Log : " + calculator.log(number1));
                     break;
                 default:
-                    System.out.println("Exiting....");
+                    System.out.println("Terminated");
                     return;
             }
         } while (true);
@@ -95,62 +86,95 @@ public class Calculator {
 
 
     public double add(double number1, double number2) {
-        logger.info("[ADDITION] - " + number1 + ", " + number2);
+        logger.info("ADDITION - " + number1 + ", " + number2);
         double result = number1 + number2;
-        logger.info("[RESULT - ADDITION] - " + result);
+        logger.info("RESULT - " + result);
         return result;
     }
 
     public double subtract(double number1, double number2) {
-        logger.info("[SUBTRACTION] - " + number1 + ", " + number2);
+        logger.info("SUBTRACTION - " + number1 + ", " + number2);
         double result = number1 - number2;
-        logger.info("[RESULT - SUBTRACTION] - " + result);
+        logger.info("RESULT - " + result);
         return result;
     }
 
 
     public double multiply(double number1, double number2) {
-        logger.info("[MULTIPLICATION] - " + number1 + ", " + number2);
+        logger.info("MULTIPLICATION - " + number1 + ", " + number2);
         double result = number1 * number2;
-        logger.info("[RESULT - MULTIPLICATION] - " + result);
+        logger.info("RESULT - " + result);
         return result;
     }
 
     public double divide(double number1, double number2) {
         double result = 0;
         try {
-            logger.info("[DIVISION] - " + number1 + ", " + number2);
+            logger.info("DIVISION - " + number1 + ", " + number2);
             if (number1 == 0 && number2 == 0) {
                 result = Double.NaN;
-                throw new ArithmeticException("Case of NaN 0.0/0.0");
+                throw new ArithmeticException("Case of 0.0/0.0");
             } else if (number1 > 0 && number2 == 0) {
                 result = Double.POSITIVE_INFINITY;
-                throw new ArithmeticException("Case of Positive Infinity 1.0/0.0");
+                throw new ArithmeticException("Case of 1.0/0.0");
             } else if (number1 <= -1 && number2 == 0) {
                 result = Double.NEGATIVE_INFINITY;
-                throw new ArithmeticException("Case of Negative Infinity -1.0/0.0");
+                throw new ArithmeticException("Case of -1.0/0.0");
             } else {
                 result = number1 / number2;
             }
         } catch (ArithmeticException error) {
-            logger.error("[EXCEPTION - DIVISION] - Cannot be divided by ZERO " + error.getLocalizedMessage());
+            logger.error("EXCEPTION - Cannot be divided by ZERO " + error.getLocalizedMessage());
         } finally {
-            logger.info("[RESULT - DIVISION] - " + result);
+            logger.info("RESULT - " + result);
         }
         return result;
     }
+
+    public double root(double number1) {
+        double result = 0;
+        double number = 0.5;
+        result = powered(number1,number);
+        return result;
+    }
+
     public double inverse(double number1) {
         double result = 0;
         double number = 1;
         result = divide(number,number1);
         return result;
     }
+
+    public double log(double number1) {
+        double result = 0;
+        result = Math.log(number1);
+        return result;
+    }
+
+    public double powered(double number1, double number2) {
+        double result = 0;
+        result = Math.pow(number1, number2);
+        try {
+            if(result == Double.NaN) {
+                throw new ArithmeticException("Case of NaN");
+            }
+            else {
+                result=result;
+            }
+        } catch (ArithmeticException error) {
+            logger.error("EXCEPTION - Cannot be found " + error.getLocalizedMessage());
+        } finally {
+            logger.info("RESULT - " + result);
+        }
+        return result;
+    }
+
     public double factorial(double number1) {
         double result = 1;
         int i = 1;
         int x = (int) number1;
         try {
-            logger.info("[FACTORIAL] - " + number1);
+            logger.info("FACTORIAL - " + number1);
             if(number1!=x) {
                 result = Double.NaN;
                 throw new ArithmeticException("Case of NaN");
@@ -164,38 +188,10 @@ public class Calculator {
                 }
             }
         } catch (ArithmeticException error) {
-            logger.error("[EXCEPTION - FACTORIAL] - Cannot be found " + error.getLocalizedMessage());
+            logger.error("EXCEPTION - Cannot be found " + error.getLocalizedMessage());
         } finally {
-            logger.info("[RESULT - FACTORIAL] - " + result);
+            logger.info("RESULT - " + result);
         }
-        return result;
-    }
-    public double powered(double number1, double number2) {
-        double result = 0;
-        result = Math.pow(number1, number2);
-        try {
-            if(result == Double.NaN) {
-                throw new ArithmeticException("Case of NaN");
-            }
-            else {
-                result=result;
-            }
-        } catch (ArithmeticException error) {
-            logger.error("[EXCEPTION - POWER] - Cannot be found " + error.getLocalizedMessage());
-        } finally {
-            logger.info("[RESULT - POWER] - " + result);
-        }
-        return result;
-    }
-    public double root(double number1) {
-        double result = 0;
-        double number = 0.5;
-        result = powered(number1,number);
-        return result;
-    }
-    public double log(double number1) {
-        double result = 0;
-        result = Math.log(number1);
         return result;
     }
 }
