cb3eca9bfec0e072a4c494d5c6daeca5af1d6939
Manan <43756669+manan-bansal@users.noreply.github.com>
2022-04-18 14:03:42 +0530

    Update Calculator.java

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -22,7 +22,7 @@ public class Calculator {
         double number1, number2 = 0;
         do {
             System.out.println("Scientific Calculator : Press the number corresponding to the operation that you need to perform");
-            System.out.print("1) Addition\n2) Subtraction\n3) Multiplication\n4) Division\n5) Inverse\n6) Factorial\n7) Power\n8) Square Root\n9) Log\n" +
+            System.out.print("1) Factorial\n2) Power\n3) Square Root\n4) Log\n" +
                     "Press any other key to exit");
             int choice;
             try {
@@ -30,7 +30,7 @@ public class Calculator {
             } catch (InputMismatchException error) {
                 return;
             }
-            if((choice==5) || (choice==6) || (choice==8) || (choice==9)) {
+            if((choice==1) || (choice==3) || (choice==4)) {
                 try{// do subtraction
                     System.out.print("Enter the number : ");
                     number1 = scanner.nextDouble();
@@ -50,31 +50,31 @@ public class Calculator {
                 }
             }
             switch (choice) {
+//                 case 1:
+//                     System.out.println("Addition : " + calculator.add(number1, number2));
+//                     break;
+//                 case 2:
+//                     System.out.println("Subtraction : " + calculator.subtract(number1, number2));
+//                     break;
+//                 case 3:
+//                     System.out.println("Multiplication : " + calculator.multiply(number1, number2));
+//                     break;
+//                 case 4:
+//                     System.out.println("Division result : " + calculator.divide(number1, number2));
+//                     break;
+//                 case 5:
+//                     System.out.println("Inverse result : " + calculator.inverse(number1));
+//                     break;
                 case 1:
-                    System.out.println("Addition : " + calculator.add(number1, number2));
-                    break;
-                case 2:
-                    System.out.println("Subtraction : " + calculator.subtract(number1, number2));
-                    break;
-                case 3:
-                    System.out.println("Multiplication : " + calculator.multiply(number1, number2));
-                    break;
-                case 4:
-                    System.out.println("Division result : " + calculator.divide(number1, number2));
-                    break;
-                case 5:
-                    System.out.println("Inverse result : " + calculator.inverse(number1));
-                    break;
-                case 6:
                     System.out.println("Factorial result : " + calculator.factorial(number1));
                     break;
-                case 7:
+                case 2:
                     System.out.println("X power Y : " + calculator.powered(number1, number2));
                     break;
-                case 8:
+                case 3:
                     System.out.println("Square root : " + calculator.root(number1));
                     break;
-                case 9:
+                case 4:
                     System.out.println("Log : " + calculator.log(number1));
                     break;
                 default:
@@ -85,51 +85,51 @@ public class Calculator {
     }
 
     // Functions
-    public double add(double number1, double number2) {
-        logger.info("ADDITION - " + number1 + ", " + number2);
-        double result = number1 + number2;
-        logger.info("RESULT - " + result);
-        return result;
-    }
+//     public double add(double number1, double number2) {
+//         logger.info("ADDITION - " + number1 + ", " + number2);
+//         double result = number1 + number2;
+//         logger.info("RESULT - " + result);
+//         return result;
+//     }
 
-    public double subtract(double number1, double number2) {
-        logger.info("SUBTRACTION - " + number1 + ", " + number2);
-        double result = number1 - number2;
-        logger.info("RESULT - " + result);
-        return result;
-    }
+//     public double subtract(double number1, double number2) {
+//         logger.info("SUBTRACTION - " + number1 + ", " + number2);
+//         double result = number1 - number2;
+//         logger.info("RESULT - " + result);
+//         return result;
+//     }
 
 
-    public double multiply(double number1, double number2) {
-        logger.info("MULTIPLICATION - " + number1 + ", " + number2);
-        double result = number1 * number2;
-        logger.info("RESULT - " + result);
-        return result;
-    }
+//     public double multiply(double number1, double number2) {
+//         logger.info("MULTIPLICATION - " + number1 + ", " + number2);
+//         double result = number1 * number2;
+//         logger.info("RESULT - " + result);
+//         return result;
+//     }
 
-    public double divide(double number1, double number2) {
-        double result = 0;
-        try {
-            logger.info("DIVISION - " + number1 + ", " + number2);
-            if (number1 == 0 && number2 == 0) {
-                result = Double.NaN;
-                throw new ArithmeticException("Case of 0.0/0.0");
-            } else if (number1 > 0 && number2 == 0) {
-                result = Double.POSITIVE_INFINITY;
-                throw new ArithmeticException("Case of 1.0/0.0");
-            } else if (number1 <= -1 && number2 == 0) {
-                result = Double.NEGATIVE_INFINITY;
-                throw new ArithmeticException("Case of -1.0/0.0");
-            } else {
-                result = number1 / number2;
-            }
-        } catch (ArithmeticException error) {
-            logger.error("EXCEPTION - Cannot be divided by ZERO " + error.getLocalizedMessage());
-        } finally {
-            logger.info("RESULT - " + result);
-        }
-        return result;
-    }
+//     public double divide(double number1, double number2) {
+//         double result = 0;
+//         try {
+//             logger.info("DIVISION - " + number1 + ", " + number2);
+//             if (number1 == 0 && number2 == 0) {
+//                 result = Double.NaN;
+//                 throw new ArithmeticException("Case of 0.0/0.0");
+//             } else if (number1 > 0 && number2 == 0) {
+//                 result = Double.POSITIVE_INFINITY;
+//                 throw new ArithmeticException("Case of 1.0/0.0");
+//             } else if (number1 <= -1 && number2 == 0) {
+//                 result = Double.NEGATIVE_INFINITY;
+//                 throw new ArithmeticException("Case of -1.0/0.0");
+//             } else {
+//                 result = number1 / number2;
+//             }
+//         } catch (ArithmeticException error) {
+//             logger.error("EXCEPTION - Cannot be divided by ZERO " + error.getLocalizedMessage());
+//         } finally {
+//             logger.info("RESULT - " + result);
+//         }
+//         return result;
+//     }
 
     public double root(double number1) {
         double result = 0;
@@ -138,12 +138,12 @@ public class Calculator {
         return result;
     }
 
-    public double inverse(double number1) {
-        double result = 0;
-        double number = 1;
-        result = divide(number,number1);
-        return result;
-    }
+//     public double inverse(double number1) {
+//         double result = 0;
+//         double number = 1;
+//         result = divide(number,number1);
+//         return result;
+//     }
 
     public double log(double number1) {
         double result = 0;
